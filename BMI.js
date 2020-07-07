f2bcd67316a1ae6251e783ab1a155dbb53cb41bf
fumio68 <rgkk.5123@gmail.com>
2020-07-07 16:54:21 +0900

    add getAdvice-Method

--- BMI.js
+++ BMI.js
@@ -18,4 +18,30 @@ class BMI {
     // BMIをリターン。
     return bmi;
   }
+
+  // アドバイスを生成するメソッド。
+  getAdvice() {
+    // BMIを計算
+    let bmi = this.weight / Math.pow(this.height/100, 2);
+    bmi = Math.round(bmi);
+    // 理想体重を計算
+    let idealWeight = 22 * Math.pow(this.height/100, 2);
+
+    // 理想体重の整数値を作成
+    idealWeight = 22 * Math.pow(this.height/100, 2);
+
+    // アドバイス用の変数を用意
+    let adviceMessage = "理想体重です。現状を維持しましょう！";
+
+    // 条件分岐
+    if(bmi > 22) {
+      adviceMessage = "太っています。" + idealWeight + "kgを目指しましょう。";
+    } else if(bmi < 22) {
+      adviceMessage = "痩せています。" + idealWeight + "kgを目指しましょう。";
+    }
+
+    // アドバイス文字列をリターン。
+    return adviceMessage;
+
+  }
 }
